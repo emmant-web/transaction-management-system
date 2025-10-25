d0ec140047c92f49ed162ea90a9f93a6abb2a2a5
emmant-web <emmanueldtoribio@gmail.com>
2025-10-25 20:46:37 +0800

    added modal feature with design and account number validation, added design to transaction table, imported Nunito font for design

---
+++ frontend/src/components/AddTransactionModal.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+function AddTransactionModal({ isOpen, onClose, onAdd }) {
+  const [form, setform] = useState({
+    transaction_date: "",
+    account_number: "",
+    account_holder_name: "",
+    amount: "",
+  });
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAdd(form);
+    setform({
+      transaction_date: "",
+      account_number: "",
+      account_holder_name: "",
+      amount: "",
+    });
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/20 backdrop-blur-sm flex justify-center items-center">
+      <div className="bg-white p-6 rounded-xl shadow-lg w-96">
+        <h2 className="text-xl font-bold">Add Transaction</h2>
+        <p className="text-gray-400 mb-4">Please fill in your details</p>
+
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            type="date"
+            name="transaction_date"
+            value={form.transaction_date}
+            onChange={handleChange}
+            className="border border-gray-300 px-4 py-2 w-full rounded-full"
+            required
+          />
+          <input
+            type="text"
+            name="account_number"
+            placeholder="1234-1234-1234"
+            value={form.account_number}
+            onChange={handleChange}
+            className="border border-gray-300 px-4 py-2 w-full rounded-full outline-none"
+            pattern="\d{4}-\d{4}-\d{4}"
+            title="Enter account number in format 1234-1234-1234"
+            required
+          />
+          <input
+            type="text"
+            name="account_holder_name"
+            placeholder="Account Holder Name"
+            value={form.account_holder_name}
+            onChange={handleChange}
+            className="border border-gray-300 px-4 py-2 w-full rounded-full"
+            required
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            value={form.amount}
+            onChange={handleChange}
+            onBlur={(e) => {
+              const value = parseFloat(e.target.value);
+              if (!isNaN(value)) {
+                setform({ ...form, amount: value.toFixed(2) });
+              }
+            }}
+            className="border border-gray-300 px-4 py-2 w-full rounded-full"
+            required
+          />
+
+          <div className="flex justify-end space-x-2">
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-4 py-2 bg-gray-200 hover:bg-gray-400 rounded-full"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-[#8370FE] text-white rounded-full hover:bg-[#594BAF]"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AddTransactionModal;
--- frontend/src/pages/TransactionsPage.jsx
+++ frontend/src/pages/TransactionsPage.jsx
@@ -1,23 +1,78 @@
 import { useState, useEffect } from "react";
 import TransactionTable from "../components/TransactionTable";
+import AddTransactionModal from "../components/AddTransactionModal";
 
-function TransactionsPage(){
-    const [transactions, setTransactions] =  useState([]);
+function TransactionsPage() {
+  const [transactions, setTransactions] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // Fetch all transactions in transactions.csv
-    useEffect(() => {
-        fetch("http://localhost:3000/api/v1/transactions")
-        .then((res) => res.json())
-        .then((data) => setTransactions(data))
-        .catch((err) => console.error(err))
-    }, [])
+  // Fetch all transactions in transactions.csv
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/transactions")
+      .then((res) => res.json())
+      .then((data) => setTransactions(data))
+      .catch((err) => console.error(err));
+  }, []);
 
-    return(
-        <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Transaction Management System</h1>
-            <TransactionTable transactions={transactions} />
+  // Add new transaction
+  const handleAddTransaction = (newTransaction) => {
+    fetch("http://localhost:3000/api/v1/transactions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTransaction),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        // Re-fetch after adding
+        return fetch("http://localhost:3000/api/v1/transactions");
+      })
+      .then((res) => res.json())
+      .then((data) => setTransactions(data))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="p-6">
+      <div className="flex justify-between mb-4">
+
+        <div>
+          <h1 className="text-3xl font-bold">
+          Transaction Management System
+        </h1>
+        <p className="text-gray-400">Overview of transactions</p>
         </div>
-    )
+        
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="text-white px-4 py-2 rounded-full flex justify-center items-center gap-2 mb-4 bg-[#8370FE]"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          Add Transaction
+        </button>
+      </div>
+
+      <TransactionTable transactions={transactions} />
+
+      <AddTransactionModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onAdd={handleAddTransaction}
+      />
+    </div>
+  );
 }
 
-export default TransactionsPage
+export default TransactionsPage;
